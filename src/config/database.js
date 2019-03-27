d6ee698a4e220b9c2be219617bf1858f6380ca81
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-26 22:15:52 -0300

    Initial sequelize config

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgress',
+  host: '127.0.0.1',
+  username: 'postgre',
+  password: 'postgre',
+  database: 'gobarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
