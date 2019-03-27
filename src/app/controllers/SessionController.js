b55acd354e80ad80063ca8ad61a190d3dd6d99af
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-27 20:52:36 -0300

    Password hash validation

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,27 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create(req, res) {
+    return res.render('auth/signin')
+  }
+
+  async store(req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.error('User not found')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.error('Incorrect password')
+      return res.redirect('/')
+    }
+
+    return res.redirect('/app/dashboard')
+  }
+}
+
+module.exports = new SessionController()
