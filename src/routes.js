60a13ecf08bd7f5cd45564a67173a5ae3c1d69f7
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-27 18:19:50 -0300

    Initial nunjucks files and config

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  res.send('Initial route')
+  res.render('auth/signup')
 })
 
 module.exports = routes
