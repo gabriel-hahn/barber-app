4c5c07c1f2377fbe146a6aeb98e0ae6c2c998381
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-28 19:28:32 -0300

    Initial user session control

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.get('/app/dashboard', (req, res) => {
-  res.render('dashboard')
+  console.log(req.session.user)
 })
 
 module.exports = routes
