043510122a3829b269e5d09e7a46bcc7786c0480
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-28 20:51:31 -0300

    All barbers list

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
 
 const authMiddleware = require('./app/middlewares/auth')
 const guestMiddleware = require('./app/middlewares/guest')
@@ -28,8 +29,6 @@ routes.use('/app', authMiddleware)
 
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
