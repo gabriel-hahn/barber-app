e615675642e0a2818ff40ab98b71512ba5cc11df
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-04-01 22:18:09 -0300

    Schedule route and controller

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,28 @@
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
 
 class AppointmentController {
   async create(req, res) {
     const provider = await User.findByPk(req.params.provider)
 
     return res.render('appointments/create', { provider })
+  }
+
+  async store(req, res) {
+    const { id } = req.session.user
+    const { provider } = req.params
+    const { date } = req.body
+
+    console.log('id: ', id)
+    console.log('provider: ', provider)
+    console.log('date: ', date)
+
+    await Appointment.create({
+      user_id: id,
+      provider_id: provider,
+      date
+    })
+
+    return res.redirect('/app/dashboard')
   }
 }
 
